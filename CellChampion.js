21ecab7b6454a4647632a52d2256b599806763d1
Raul <raul.lucero27@gmail.com>
2015-05-04 23:22:40 -0500

    [add] se agrega seccion de formato y impresion de datos de la fila de campeon

---
+++ CellChampion.js
@@ -0,0 +1,63 @@
+var React = require('react-native');
+
+var REQUEST_IMAGE_CHAMP_SMALL = 'http://ddragon.leagueoflegends.com/cdn/5.8.1/img/champion/';
+
+var {
+  View,
+  Image,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+} = React;
+
+CellChampion = React.createClass({
+
+  render: function() {
+    var urlImage = REQUEST_IMAGE_CHAMP_SMALL + this.props.champion.image.full;
+    return (
+      <View>
+        <TouchableHighlight onPress={this.props.onSelect}>
+          <View style={styles.container}>
+            <Image
+              style={styles.image}
+              source={{uri: urlImage}}
+            />
+            <View style={styles.rightContainer}>
+              <Text style={styles.name}>{this.props.champion.name}</Text>
+              <Text style={styles.title}>{this.props.champion.title}</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  },
+
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    borderWidth: .75,
+    borderColor: '#000000',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  title: {
+    textAlign: 'center',
+  },
+  image: {
+    width: 60,
+    height: 60,
+  },
+});
+
+module.exports = CellChampion;
