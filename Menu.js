d676166de6c5f1fe85d22749c97719466b05c8c7
Raul <raul.lucero27@gmail.com>
2015-05-06 18:35:53 -0500

    [add] se agrega archivo menu donde se describen secciones
    Esto sirve para listar las diferentes secciones de la aplicacion y asi mismo declarar los metodos de llamadas de las funciones dependientes de las secciones

---
+++ Menu.js
@@ -0,0 +1,69 @@
+var React = require('react-native');
+var AllChampions = require('./AllChampions.js')
+var {
+  View,
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+} = React;
+
+var Menu = React.createClass({
+  onSelectChampions: function() {
+    this.props.navigator.push({
+      title: 'All Champions',
+      component: AllChampions,
+    });
+  },
+
+  onSelectSummoner: function() {
+    // TODO Aqui va la llamada a la funcion de inicio de busqueda
+    // de datos de summoner
+  },
+  render: function() {
+    return (
+        <View>
+          <TouchableHighlight onPress={this.onSelectChampions}>
+            <View style={styles.container}>
+              <Text style={styles.name}>Champion</Text>
+            </View>
+          </TouchableHighlight>
+          <TouchableHighlight onPress={this.onSelectSummoner}>
+            <View style={styles.container}>
+              <Text style={styles.name}>Summoner</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
+
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    borderWidth: .75,
+    marginTop: 120,
+    borderColor: '#000000',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  title: {
+    textAlign: 'center',
+  },
+  image: {
+    width: 60,
+    height: 60,
+  },
+
+});
+
+module.exports = Menu;
