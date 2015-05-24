55b86fc69c9a25206c47c4eaf719e944fdfb8a7e
Raul <raul.lucero27@gmail.com>
2015-05-23 20:36:06 -0500

    [add] se añade archivo para funciones y vista de busqeda de campeones
    Se añadio el archivo SearchChampion aqui se integraran los metodos de busqueda y el diseño de la interfaz para el input de busqueda

---
+++ SearchChampion.js
@@ -0,0 +1,48 @@
+var React = require('react-native');
+
+var {
+  View,
+  TextInput,
+  ActivityIndicatorIOS,
+  StyleSheet,
+} = React;
+
+'use strict';
+
+var SearchChampion = React.createClass({
+
+  render: function() {
+    return (
+      <View
+        style={styles.searchBar} >
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          //onChange={this.props.onSearchChange}
+          placeholder="Search a champion"
+          //onFocus={this.props.onFocus}
+          style={styles.searchBarInput}
+        />
+
+      </View>
+    );
+  }
+
+});
+
+var styles = StyleSheet.create({
+  searchBar: {
+    marginTop: 64,
+    padding: 3,
+    paddingLeft: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchBarInput: {
+    height: 36,
+    fontSize: 18,
+    flex: 1,
+  },
+});
+
+module.exports = SearchChampion;
